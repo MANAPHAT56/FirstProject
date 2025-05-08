41b53309eb8860931de7cfc34328126c2bc5fc9d
MANAPHAT56 <ttpho5874@gmail.com>
2025-05-08 15:41:46 +0700

    gk

--- TaskTracker-nginx/AmazonS3.js
+++ TaskTracker-nginx/AmazonS3.js
@@ -1,35 +1,35 @@
-import AWS from "aws-sdk";
-import fs from "fs";
+// import AWS from "aws-sdk";
+// import fs from "fs";
 
-// ตั้งค่า AWS SDK
-const s3 = new AWS.S3({
-  accessKeyId: "YOUR_ACCESS_KEY",
-  secretAccessKey: "YOUR_SECRET_KEY",
-  region: "ap-southeast-1" // เปลี่ยนเป็นโซนที่เลือก
-});
+// // ตั้งค่า AWS SDK
+// const s3 = new AWS.S3({
+//   accessKeyId: "YOUR_ACCESS_KEY",
+//   secretAccessKey: "YOUR_SECRET_KEY",
+//   region: "ap-southeast-1" // เปลี่ยนเป็นโซนที่เลือก
+// });
 
-async function uploadFile(filePath, bucketName) {
-  const fileContent = fs.readFileSync(filePath);
-  const params = {
-    Bucket: bucketName,
-    Key: "uploads/" + filePath.split("/").pop(), // ตั้งชื่อไฟล์
-    Body: fileContent,
-    ACL: "public-read" // ตั้งให้ไฟล์เป็นสาธารณะ
-  };
+// async function uploadFile(filePath, bucketName) {
+//   const fileContent = fs.readFileSync(filePath);
+//   const params = {
+//     Bucket: bucketName,
+//     Key: "uploads/" + filePath.split("/").pop(), // ตั้งชื่อไฟล์
+//     Body: fileContent,
+//     ACL: "public-read" // ตั้งให้ไฟล์เป็นสาธารณะ
+//   };
 
-  try {
-    const result = await s3.upload(params).promise();
-    console.log("File uploaded:", result.Location);
-    return result.Location;
-  } catch (err) {
-    console.error("Upload error:", err);
-  }
-}
-function getFileUrl(bucketName, fileName) {
-    return `https://${bucketName}.s3.amazonaws.com/uploads/${fileName}`;
-  }
+//   try {
+//     const result = await s3.upload(params).promise();
+//     console.log("File uploaded:", result.Location);
+//     return result.Location;
+//   } catch (err) {
+//     console.error("Upload error:", err);
+//   }
+// }
+// function getFileUrl(bucketName, fileName) {
+//     return `https://${bucketName}.s3.amazonaws.com/uploads/${fileName}`;
+//   }
   
-  console.log(getFileUrl("my-image-bucket", "test.jpg"));
+//   console.log(getFileUrl("my-image-bucket", "test.jpg"));
   
-// ใช้งาน
-uploadFile("test.jpg", "my-image-bucket");
+// // ใช้งาน
+// uploadFile("test.jpg", "my-image-bucket");
