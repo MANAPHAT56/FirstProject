0ca49b32862c5381534191401d434a6e08a3bcc6
MANAPHAT56 <ttpho5874@gmail.com>
2025-05-22 10:36:19 +0700

    า

--- reverseproxy/app.js
+++
@@ -1,9 +0,0 @@
-const express=require("express");
-const os =require("os");
-const app=express();
-app.use("/",async (req,res)=>{
-  res.json({message:"Response from: ",hostname:`${os.hostname}`})
-})
-app.listen(3000,()=>{
-    console.log("HI iamrunning");
-})
